52f9710370305b7e0455e73349868a1808b043d3
jordan13 <jordan15cg@gmail.com>
2026-06-26 15:35:34 -0500

    part2 django

--- home/views.py
+++ home/views.py
@@ -6,3 +6,5 @@ from .models import Mensagem
 def index(request):
     mensagens = Mensagem.objects.all()
     return render(request, "home/index.html", {"mensagens": mensagens})
+def sobre(request):                                   # ← nova view
+    return render(request, "home/sobre.html")
